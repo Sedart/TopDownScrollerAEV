4c99b43a2b5131d0d016b8b58c8eebf15defb7e4
Sedart <davidfmpixel@gmail.com>
2014-11-29 21:06:06 +0100

    - Added ground texture - Added basic movement and aim control - Added more PSD for textures

--- UnityProjectTPDSAEV/Assets/Scripts/PlayerController.cs
+++ UnityProjectTPDSAEV/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	public float playerMoveSpeed;
+	public float playerRotateSpeed;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +13,16 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//Move player when input axis are modified from -1 to +1
+		//to achieve an smooth movement
+
+		transform.Translate (	Input.GetAxis ("Horizontal") * playerMoveSpeed * Time.deltaTime,
+		                    	Input.GetAxis ("Vertical") * playerMoveSpeed * Time.deltaTime,
+		                    	0.0f,
+		                     	Space.World);
+
+		//Rotate the player to aim using mouse position
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
 	}
 }
